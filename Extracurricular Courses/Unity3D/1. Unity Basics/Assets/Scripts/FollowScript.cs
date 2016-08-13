b1e2e93622d65201b1b81ab094ff7faccb4bee8c
YouJinTou <dandreevd@gmail.com>
2016-08-13 14:02:50 +0300

    Added the first homework assignment for the Unity course

---
+++ Extracurricular Courses/Unity3D/1. Unity Basics/Assets/Scripts/FollowScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class FollowScript : MonoBehaviour
+{
+    public Transform Guide;
+    public float FollowSpeed;
+    public float MinDistance;
+
+    private float distance;
+
+    void Awake()
+    {
+        this.distance = (this.FollowSpeed * Time.deltaTime);
+    }
+
+    void LateUpdate()
+    {
+        if (this.Guide == null)
+        {
+            return;
+        }
+
+        this.transform.LookAt(this.Guide);
+
+        if (Vector3.Distance(this.transform.position, this.Guide.position) > this.MinDistance)
+        {
+            this.transform.Translate(0f, 0f, this.distance);
+        }        
+    }
+}
---
+++ Extracurricular Courses/Unity3D/1. Unity Basics/Assets/Scripts/MoveScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+    public float Speed;
+
+    private float distance;
+
+    void Awake()
+    {
+        this.distance = (Time.deltaTime * this.Speed);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.Translate(0f, this.distance, 0f);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.Translate(0f, -this.distance, 0f);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Translate(-this.distance, 0f, 0f);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Translate(this.distance, 0f, 0f);
+        }
+    }
+}
