568915489bd40afb064c225759a07dbb9eacd42f
YouJinTou <dandreevd@gmail.com>
2016-08-15 21:20:01 +0300

    Added 3. Unity Collision and Physics

---
+++ Extracurricular Courses/Unity3D/3. Unity Collision and Physics/Collision and Physics/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private const float EarthAcceleration = 9.81f;
+    private const float AccelerationInAir = 19.82f;
+    private const float JumpForce = 3f;
+    private const float SpeedCoeff = 2.5f;
+    private const float MouseSpeedX = 1f;
+    private const float MouseSpeedY = 1f;
+
+    private Rigidbody heroRigidBody;
+    private Camera povCamera;
+    private bool isGrounded;
+
+    void Start()
+    {
+        Physics.gravity = Vector3.down * EarthAcceleration;
+        this.heroRigidBody = GetComponent<Rigidbody>();
+        this.povCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        this.UpdateSpatialMovement();
+        this.UpdateMouseMovement();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            this.Jump();
+        }
+    }    
+
+    private void UpdateSpatialMovement()
+    {
+        Vector3 velocityVector = new Vector3(this.heroRigidBody.velocity.x, this.heroRigidBody.velocity.z, 0f); // Can't go up the y-axis
+        this.heroRigidBody.velocity = new Vector3(velocityVector.x, this.heroRigidBody.velocity.y, velocityVector.y);
+
+        this.heroRigidBody.AddRelativeForce(Input.GetAxis("Horizontal") * SpeedCoeff, 0f, Input.GetAxis("Vertical") * SpeedCoeff, ForceMode.Force);
+    }
+
+    private void UpdateMouseMovement()
+    {
+        float mouseHorizontal = (this.transform.rotation.eulerAngles.y + (Input.GetAxis("Mouse X") * MouseSpeedY));
+        float mouseVertical = (this.povCamera.transform.localRotation.eulerAngles.x - (Input.GetAxis("Mouse Y") * MouseSpeedX));
+        this.transform.rotation = Quaternion.Euler(0f, mouseHorizontal, 0f); // parent
+        this.povCamera.transform.localRotation = Quaternion.Euler(mouseVertical, 0f, 0f); // child
+    }
+
+    private void Jump()
+    {
+        if (!this.isGrounded) // We are in the air; can't jump again
+        {
+            return;
+        }
+
+        this.heroRigidBody.AddForce(0f, EarthAcceleration * JumpForce, 0f);
+    }
+
+    private void SetGravity(int type)
+    {
+        Physics.gravity = Vector3.down * ((type == 1) ? EarthAcceleration : AccelerationInAir);
+    }
+
+    private void OnTriggerStay(Collider col)
+    {
+        if (col.tag == "Floor" && !this.isGrounded)
+        {
+            this.isGrounded = true;
+        }
+    }
+
+    private void OnTriggerEnter (Collider col)
+    {
+        if (col.tag == "Floor" && !this.isGrounded)
+        {
+            this.SetGravity(1);
+
+            this.isGrounded = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        if (col.tag == "Floor" && this.isGrounded)
+        {
+            this.SetGravity(2);
+
+            this.isGrounded = false;
+        }
+    }
+}
---
+++ Extracurricular Courses/Unity3D/3. Unity Collision and Physics/Collision and Physics/Assets/Scripts/Shooting.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    private const float BulletSpeed = 200f;
+
+    public GameObject BulletHoleSprite;
+    public GameObject Bullet;
+    public GameObject Rifle;
+
+    private Camera povCamera;
+    private bool isInBulletHoleMode;
+
+    void Start()
+    {
+        this.povCamera = Camera.main;
+        this.isInBulletHoleMode = false;
+    }
+
+    void Update()
+    {
+        //if (Input.GetKey(KeyCode.Alpha1))
+        //{
+        //    this.isInBulletHoleMode = true;
+        //}
+        //else if (Input.GetKey(KeyCode.Alpha2))
+        //{
+        //    this.isInBulletHoleMode = false;
+        //}
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (this.isInBulletHoleMode)
+            {
+                Ray ray = this.povCamera.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit, 100f))
+                {
+                    GameObject bulletHole = Instantiate(this.BulletHoleSprite);
+
+                    bulletHole.transform.position = hit.point;
+                    bulletHole.transform.rotation = hit.collider.transform.rotation;
+                }
+            }
+            else
+            {
+                Rigidbody bullet = Instantiate(this.Bullet).GetComponent<Rigidbody>();
+                bullet.transform.position = this.Rifle.transform.position; // ?????
+
+                bullet.AddRelativeForce(Vector3.forward * BulletSpeed);
+            }
+        }
+    }
+}
